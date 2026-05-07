7bf1f06dbf755bdf9e15dbb98e78268407ceb536
Peter Kröner <peter.kroner@storyblok.com>
2026-05-07 13:40:45 +0200

    force dev mode when previewing drafts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  dev: process.env.CONTENT_VERSION === "draft" ? true : undefined,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   i18n: {
